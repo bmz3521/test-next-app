2e7ae652cfd5cf282b204fd779b3a82f395b7b82
anurak <anurak@everapp.io>
2023-02-13 02:36:56 +0700

    fix props

--- pages/store/price/saga.ts
+++ pages/store/price/saga.ts
@@ -1,14 +1,4 @@
 import axios from 'axios';
-import { all, call, put, takeLatest } from 'redux-saga/effects';
-
-import {
-    priceSuccess,
-    priceFailure,
-} from './actions';
-import { PriceState } from './types';
-import { 
-    GET_PRICE_REQUEST,
-} from './actionTypes';
 
 export const getPrice = async (params:{ symbol:string }) => {
     const { data } = await axios.get<PriceState>(
@@ -18,26 +8,3 @@ export const getPrice = async (params:{ symbol:string }) => {
         
         return data
     }
-    
-    //------------------------------------------ saga --------------------------------
-    
-    function* priceRequestSaga(action:any){
-    console.log('action ===',action)
-    try{
-        const response:{data:PriceState} = yield call(getPrice, {
-            symbol : action.params.symbol
-        });
-        console.log('response naja =',response)
-        yield put(priceSuccess(response));
-    }catch (e : any) {
-        yield put(priceFailure({
-            error : e
-        }))
-    }
-}
-
-function* priceSaga() {
-    yield all([takeLatest(GET_PRICE_REQUEST, priceRequestSaga)])
-}
-
-export default priceSaga;
--- pages/store/rootSaga.ts
+++
@@ -1,7 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-
-import priceSaga from './price/saga';
-
-export function* rootSaga(){
-    yield all([fork(priceSaga)]);
-}
