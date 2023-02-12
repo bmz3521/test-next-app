986a4dbf453784102708f133cf05e629b2a7bf92
anurak <anurak@everapp.io>
2023-02-13 02:37:46 +0700

    fix props

--- pages/store/price/saga.ts
+++ pages/store/price/saga.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getPrice = async (params:{ symbol:string }) => {
-    const { data } = await axios.get<PriceState>(
+    const { data } = await axios.get(
         `https://satangcorp.com/api/v3/ticker/24hr?symbol=${params.symbol || 'btc_thb'}`,
         );
         // console.log('data ===',data)
