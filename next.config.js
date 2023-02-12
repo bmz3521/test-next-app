d985dcefc15e7fabf6dc2c301281c867b08b762c
anurak <anurak@everapp.io>
2023-02-13 00:45:11 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+const { redirect } = require('next/dist/server/api-utils')
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: process.env.REACT_APP_BASE_REDIRECT_PATH,
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
